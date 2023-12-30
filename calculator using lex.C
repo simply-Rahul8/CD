7c973a158983b03d232c74d9e59866a79fc32dbb
simply-Rahul8 <154051568+simply-Rahul8@users.noreply.github.com>
2023-12-30 18:30:04 +0530

    Add files via upload

---
+++ calculator using lex.C
@@ -0,0 +1,48 @@
+%{
+#include<stdio.h>
+int op = 0, i;
+float a, b;
+void digi();
+%}
+%%
+[0-9]+|([0-9]*)"."([0-9]+) digi();
+"+" op = 1;
+"-" op = 2;
+"*" op = 3;
+"/" op = 4;
+"^" op = 5;
+";" exit(0);
+\n printf("The Answer : %f\nEnter expression : ", a);
+%%
+void digi() {
+if (op == 0) a = atof(yytext);
+else {
+b = atof(yytext);
+switch(op) {
+case 1: a = a + b; break;
+case 2: a = a - b; break;
+case 3: a = a * b; break;
+case 4: a = a / b; break;
+case 5: for (i = a; b > 1; b--) a = a * i; break;
+}
+op=0;
+}
+}
+int main(int argv,char *argc[]) {
+printf("\nEnter expression : ");
+yylex();
+}
+int yywrap() {
+return 1;
+}
+
+// EXECUTION:
+// flex calculatorLEX.l
+// gcc lex.yy.c -o calculator -ll
+// ./calculator
+
+
+
+
+//o: Enter expression:2+3
+// The Answer: 5.000000
